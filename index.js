71beadb67a3ecefbabac9d08001c2edf7e331662
AntonGoncharov1999 <syppahaka@mail.ru>
2019-04-22 18:27:25 +0500

    Two commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var express = require( 'express' ),
-	app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get( "/", function (req, res) {
-	res.send( "Hell World" )
-} );
+const app = express();
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// start server
-app.listen( 3000, function () {
-	global.console.log('Work on port: 3000!')
+const arr = ['hello', 'world', 'test'];
+
+app.get('/', (req, res) => res.render('index', { arr: arr }));
+
+app.get('/create', (req, res) => res.render('create'));
+app.post('/create', (req, res) => {
+  arr.push(req.body.text);
+  res.redirect('/');
 });
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
